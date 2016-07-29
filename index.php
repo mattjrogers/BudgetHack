96071b30a4b9d9f824e6fc54426e9bf1d5561816
mattjrogers <Matt@Matts-iMac.lan>
2016-07-29 23:36:50 +0800

    Comment 1
    Project Start

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>#BudgetHack</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bugethack.css" rel="stylesheet">
+    
+    <!-- Web Fonts -->
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans:300|Roboto:700' rel='stylesheet' type='text/css'>
+
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <div class="row">
+    
+    <div class="col-xs-12 col-md-4 col-lg-2" style="background:white;">
+    <div class="col-xs-10 col-xs-offset-1" style="background:white;">
+    	<h2>#BudgetHack</h2>
+    	<p>Examining rates of innovation in relation to local government budgets</p>
+        
+    	<ul class="navigation">
+    		<li><a href="#">About</a></li>
+       		<li><a href="#">Demo</a></li>
+    	</ul>
+    	</div>
+        
+    </div>
+    <div class="col-xs-12 col-md-8 col-lg-10" style="background:lightgrey;">
+    Content
+    </div>
+ 
+	</div>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
